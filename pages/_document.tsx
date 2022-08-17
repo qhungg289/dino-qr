afe5b7ea22f4b0eba1d8644badb5bba3d897962a
Hung Duong Quang <qhungg289@protonmail.com>
2022-08-17 23:27:49 +0700

    Init commit

---
+++ pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+	return (
+		<Html>
+			<Head>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;500;700&display=swap"
+					rel="stylesheet"
+				/>
+			</Head>
+			<body className="container font-nunito">
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
+}
