712134cdd32a376d8a1028e393f17ccb4b29caf3
Hung Duong Quang <qhungg289@protonmail.com>
2022-08-18 20:25:54 +0700

    Update document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,12 +2,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
 	return (
-		<Html>
+		<Html lang="en">
 			<Head>
 				<link
 					href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;500;700&display=swap"
 					rel="stylesheet"
 				/>
+				<meta name="description" content="Simple QR code generator" />
 			</Head>
 			<body className="container max-w-lg font-nunito">
 				<Main />
