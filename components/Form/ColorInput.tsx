a5711c2bbae1016aaa2aec38828e84e5ec3d8fd3
Hung Duong Quang <qhungg289@protonmail.com>
2022-08-20 23:07:51 +0700

    Update ColorInput

--- components/Form/ColorInput.tsx
+++ components/Form/ColorInput.tsx
@@ -1,4 +1,4 @@
-import { TwitterPicker } from "react-color";
+import { SliderPicker } from "react-color";
 import invert from "invert-color";
 import { motion, AnimatePresence } from "framer-motion";
 import { useEffect, useRef, useState } from "react";
@@ -37,34 +37,30 @@ export default function ColorInput({ name, id, value, onChange }: Props) {
 				style={{ backgroundColor: value, color: invert(value, true) }}
 				value={value}
 				onFocus={() => setIsPickerOpen(true)}
-				className="w-full p-3 mt-1 border-2 border-gray-200 focus:border-gray-200 focus:outline-none focus:ring-1 focus:ring-gray-200 rounded transition-colors cursor-pointer"
+				onClick={() => setIsPickerOpen(true)}
+				className="w-full p-3 mt-1 border-2 border-gray-200 focus:border-gray-200 focus:outline-none focus:ring-1 focus:ring-gray-200 rounded-lg transition-colors cursor-pointer"
 			/>
 
 			<AnimatePresence>
 				{isPickerOpen && (
 					<motion.div
-						className="absolute top-16 z-10"
+						className="absolute flex flex-col gap-4 top-16 z-10 w-full border shadow-lg rounded p-4 bg-white"
 						ref={picker}
 						initial={{ opacity: 0, y: -50 }}
 						animate={{ opacity: 1, y: 0 }}
 						exit={{ opacity: 0 }}
 					>
-						<TwitterPicker
+						<SliderPicker
 							color={value}
-							colors={[
-								"#000000",
-								"#991b1b",
-								"#115e59",
-								"#065f46",
-								"#075985",
-								"#1e40af",
-								"#3730a3",
-								"#5b21b6",
-								"#9d174d",
-								"#9f1239",
-							]}
-							onChangeComplete={(color, _) => onChange(color.hex)}
+							onChange={(color, _) => onChange(color.hex)}
 						/>
+
+						<button
+							className="text-emerald-600 font-bold px-5 py-3 rounded-lg hover:bg-emerald-100 transition-colors"
+							onClick={() => setIsPickerOpen(false)}
+						>
+							Done
+						</button>
 					</motion.div>
 				)}
 			</AnimatePresence>
