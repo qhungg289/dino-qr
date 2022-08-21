504af14318c61fbf0f483e356f6f0baa7de8d094
Hung Duong Quang <qhungg289@protonmail.com>
2022-08-21 21:05:19 +0700

    Update styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -111,21 +111,7 @@ function Home() {
 						</div>
 					</div>
 
-					<FilledButton type="submit">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							className="h-5 w-5"
-							viewBox="0 0 20 20"
-							fill="currentColor"
-						>
-							<path
-								fillRule="evenodd"
-								d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
-								clipRule="evenodd"
-							/>
-						</svg>
-						Create
-					</FilledButton>
+					<FilledButton type="submit">Generate</FilledButton>
 				</motion.form>
 			</div>
 
@@ -151,18 +137,6 @@ function Home() {
 								}
 								className="col-span-3"
 							>
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									className="h-5 w-5"
-									viewBox="0 0 20 20"
-									fill="currentColor"
-								>
-									<path
-										fillRule="evenodd"
-										d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
-										clipRule="evenodd"
-									/>
-								</svg>
 								Download
 							</FilledButton>
 
